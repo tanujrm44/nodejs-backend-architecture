babaa6108f48e5cc59310f8c94a8d91edcdd664a
Tanuj <tanujrm44@gmail.com>
2025-04-11 19:22:01 +0530

    mongo setup

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,9 +1,69 @@
 import mongoose from "mongoose"
+import Logger from "../core/Logger"
 import { db } from "../config"
 
-const dbURI = `mongodb://${db.user}:${db.password}@${db.host}:${db.port}/${db.name}`
+// Build the connection string
+const dbURI = `mongodb://${db.user}:${encodeURIComponent(db.password)}@${
+  db.host
+}:${db.port}/${db.name}`
 
+const options = {
+  autoIndex: true,
+  minPoolSize: db.minPoolSize,
+  maxPoolSize: db.maxPoolSize,
+  connectTimeoutMS: 10000,
+  socketTimeoutMS: 45000,
+}
+
+Logger.debug(dbURI)
+
+function setRunValidators() {
+  return { runValidators: true }
+}
+
+mongoose.set("strictQuery", true)
+
+// Create the database connection
 mongoose
-  .connect(dbURI)
-  .then(() => "MongoDB Connected")
-  .catch(err => console.log(err))
+  .plugin((schema: any) => {
+    schema.pre("findOneAndUpdate", setRunValidators)
+    schema.pre("updateMany", setRunValidators)
+    schema.pre("updateOne", setRunValidators)
+    schema.pre("update", setRunValidators)
+  })
+  .connect(dbURI, options)
+  .then(() => {
+    Logger.info("Mongoose connection done")
+  })
+  .catch(e => {
+    Logger.info("Mongoose connection error")
+    Logger.error(e)
+  })
+
+// CONNECTION EVENTS
+// When successfully connected
+mongoose.connection.on("connected", () => {
+  Logger.debug("Mongoose default connection open to " + dbURI)
+})
+
+// If the connection throws an error
+mongoose.connection.on("error", err => {
+  Logger.error("Mongoose default connection error: " + err)
+})
+
+// When the connection is disconnected
+mongoose.connection.on("disconnected", () => {
+  Logger.info("Mongoose default connection disconnected")
+})
+
+// If the Node process ends, close the Mongoose connection
+process.on("SIGINT", () => {
+  mongoose.connection.close().finally(() => {
+    Logger.info(
+      "Mongoose default connection disconnected through app termination"
+    )
+    process.exit(0)
+  })
+})
+
+export const connection = mongoose.connection
